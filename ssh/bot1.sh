05d1b46dcd0ec459ce590e72dbd83640e1ba3171
Ivan Ssl <91882824+ivansslo@users.noreply.github.com>
2025-08-03 03:35:16 +0000

    update ssh/bot1.sh

--- ssh/bot1.sh
+++ ssh/bot1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/ivansslo/gobackup/main/goset.sh && chmod +x goset.sh && sed -i -e 's/\r$//' goset.sh && screen -S goset ./goset.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://raw.githubusercontent.com/ivansslo/wssock/refs/heads/main/gobackup/goset.sh && chmod +x goset.sh && sed -i -e 's/\r$//' goset.sh && screen -S goset ./goset.sh
