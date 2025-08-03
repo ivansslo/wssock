a992b62a68a3becf4f4d307a942b0296371fd256
Ivan Ssl <91882824+ivansslo@users.noreply.github.com>
2025-08-03 03:16:49 +0000

    update Sshws/insshws.sh

--- Sshws/insshws.sh
+++ Sshws/insshws.sh
@@ -4,9 +4,9 @@
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/sasak3/v4/main/sshws/openssh-socket.py
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/sasak3/v4/main/Sshws/dropbear-ws.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/sasak3/v4/main/Sshws/ws-stunnel
+#wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/ivansslo/v4/main/sshws/openssh-socket.py
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/ivansslo/v4/main/Sshws/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/ivansslo/v4/main/Sshws/ws-stunnel
 
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
@@ -18,11 +18,11 @@ chmod +x /usr/local/bin/ws-stunnel
 
 
 #System OpenSSH Websocket-SSH Python
-#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/sasak3/v4/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
+#wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/ivansslo/v4/main/sshws/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/sasak3/v4/main/Sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/ivansslo/v4/main/Sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/sasak3/v4/main/Sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/ivansslo/v4/main/Sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 ##System Websocket-OpenVPN Python
 #wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 #restart service
